ec0d45a842cb335eedff58cabe7e504192126512
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-11-03 12:42:29 +0900

    修正

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,6 +2,11 @@ require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
+  setup do
+    @user = users(:one)
+    @post = posts(:one)
+    sign_in @user
+  end
 
   test "should get home" do
     get pages_home_url
